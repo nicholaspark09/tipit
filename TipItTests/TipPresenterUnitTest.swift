ee836c78f68ba43d02be6e24b13b74c6c8aa935d
vn008xw <npark@walmartlabs.com>
2017-08-19 17:21:26 -0700

    Adds mocking pod

---
+++ TipItTests/TipPresenterUnitTest.swift
@@ -0,0 +1,39 @@
+//
+//  TipPresenterUnitTest.swift
+//  TipIt
+//
+//  Created by Nicholas Park - Vendor on 8/19/17.
+//  Copyright © 2017 Nicholas Park - Vendor. All rights reserved.
+//
+
+import XCTest
+@testable import TipIt
+
+class TipPresenterUnitTest: XCTestCase {
+    
+    var mockRepository: TipRepository!
+    var presenter: TipPresenter!
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
